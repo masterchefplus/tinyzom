49f774c3bb031b96df0bdd29e6efb13c8710f5c2
masterchefplus <72683409+masterchefplus@users.noreply.github.com>
2021-11-13 18:12:20 +0700

    Add files via upload

--- precache-manifest.139e389ac0ec9b83d87d78d6f3ac1a03.js
+++ precache-manifest.139e389ac0ec9b83d87d78d6f3ac1a03.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "11964a80dc8066b9c12dd0e2d0beaecf",
-    "url": "/my-crypto-zombie/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "5020d2e047abc69b49f1",
-    "url": "/my-crypto-zombie/static/css/main.f1a3acfe.chunk.css"
+    "url": "/static/css/main.f1a3acfe.chunk.css"
   },
   {
     "revision": "092189c505e4a7d1b250",
-    "url": "/my-crypto-zombie/static/js/2.701cca23.chunk.js"
+    "url": "/static/js/2.701cca23.chunk.js"
   },
   {
     "revision": "b6da2446aaaddb21417ba711efe278a3",
-    "url": "/my-crypto-zombie/static/js/2.701cca23.chunk.js.LICENSE"
+    "url": "/static/js/2.701cca23.chunk.js.LICENSE"
   },
   {
     "revision": "5020d2e047abc69b49f1",
-    "url": "/my-crypto-zombie/static/js/main.a6dfdf39.chunk.js"
+    "url": "/static/js/main.a6dfdf39.chunk.js"
   },
   {
     "revision": "817c9e11ebfc9dd79683",
-    "url": "/my-crypto-zombie/static/js/runtime-main.3c1a62d7.js"
+    "url": "/static/js/runtime-main.3c1a62d7.js"
   },
   {
     "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
-    "url": "/my-crypto-zombie/static/media/Btn_Register_Normal.d2b2559b.png"
+    "url": "/static/media/Btn_Register_Normal.d2b2559b.png"
   },
   {
     "revision": "d3684ace4f10c62c9209b45d3bb724c2",
-    "url": "/my-crypto-zombie/static/media/Btn_Register_Over.d3684ace.png"
+    "url": "/static/media/Btn_Register_Over.d3684ace.png"
   },
   {
     "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
-    "url": "/my-crypto-zombie/static/media/homecard@2x.64a6f16c.png"
+    "url": "/static/media/homecard@2x.64a6f16c.png"
   },
   {
     "revision": "9d0437bbb5e705bad95ac929361faf3e",
-    "url": "/my-crypto-zombie/static/media/index_bg.9d0437bb.png"
+    "url": "/static/media/index_bg.9d0437bb.png"
   },
   {
     "revision": "7f9c96ea543f6c75f7dffbd6ce4b4415",
-    "url": "/my-crypto-zombie/static/media/start_btn.7f9c96ea.png"
+    "url": "/static/media/start_btn.7f9c96ea.png"
   },
   {
     "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
-    "url": "/my-crypto-zombie/static/media/tester-bg@2x.2a4a2235.png"
+    "url": "/static/media/tester-bg@2x.2a4a2235.png"
   },
   {
     "revision": "b102b8003aab95865cb7363428273a16",
-    "url": "/my-crypto-zombie/static/media/walls.b102b800.jpg"
+    "url": "/static/media/walls.b102b800.jpg"
   },
   {
     "revision": "08087132d6dcfcd5c5f471880c104712",
-    "url": "/my-crypto-zombie/static/media/zombierun.08087132.png"
+    "url": "/static/media/zombierun.08087132.png"
   }
 ]);
